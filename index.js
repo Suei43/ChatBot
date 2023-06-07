ccf468011db235778c55e85f8850bcfafbfeaa95
Suei43 <sueiraphael@gmail.com>
2023-06-07 23:55:53 +0100

    minor updates

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ require('dotenv').config();
 const app = express();
 
 const port = process.env.PORT || 3000;
+const API_KEY = process.env.OPENAI_API_KEY;
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello World" });
 });
 
 app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
+    console.log(`http://localhost:${port}`)
+    console.log(`API_KEY: ${API_KEY}`)
 })
 
 
