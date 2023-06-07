1317c84ce66d1d6efb3089349b3557cfe8a18dab
Suei43 <sueiraphael@gmail.com>
2023-06-08 00:19:20 +0100

    axios req to openai completions engine

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 require('dotenv').config();
+const openai = require("openai");
+const axios = require("axios");
 
-const app = express();
+const payload = {};
+const headers = {
+    'Authorization': `Bearer ${API_KEY}`,
+    'Content-Type': 'application/json',
+};
+
+const instance = axios.create({
+    baseURL: 'https://api.openai.com/v1/engines/davinci/completions',
+    headers: headers,
+    timeout: 2000,
+});
+
+instance.post('/', payload).then((res) => {
+    console.log(res.data);
+}).catch((err) => {
+    console.error(err);
+});
 
 const port = process.env.PORT || 3000;
 const API_KEY = process.env.OPENAI_API_KEY;
+
+const app = express();
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello World" });
